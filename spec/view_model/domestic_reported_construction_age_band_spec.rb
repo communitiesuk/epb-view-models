069a5ca9323b4d618c5f89e1a5647d11a9a2cff9
Aga Dufrat <a.dufrat@gmail.com>
2021-08-02 14:04:40 +0100

    Refactor test for domestic view model construction age band
    to avoid linting errors due to empty describe group
    https://www.rubydoc.info/gems/rubocop-rspec/RuboCop/Cop/RSpec/EmptyExampleGroup

--- spec/view_model/domestic_reported_construction_age_band_spec.rb
+++ spec/view_model/domestic_reported_construction_age_band_spec.rb
@@ -1,81 +1,82 @@
 RSpec.describe "ViewModel::Domestic::ConstructionAgeBand" do
-  context "when there is a construction age band value or construction year for the Main Dwelling" do
-    def self.retrieve_construction_age_band(schema, type, construction_option)
-      it "returns the value for construction age band or construction year" do
-        document = Nokogiri.XML Samples.xml(schema, type)
-        wrapper = ViewModel::Factory.new.create(document.to_s, schema.to_s)
-        age_band = wrapper.view_model.main_dwelling_construction_age_band_or_year
+  test_data =
+    [
+      { schema: "SAP-Schema-18.0.0", type: "epc", construction_option: "1750" },
+      { schema: "SAP-Schema-17.1", type: "epc", construction_option: "1750" },
+      { schema: "SAP-Schema-17.0", type: "epc", construction_option: "1750" },
+      { schema: "SAP-Schema-16.3", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-16.3", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-16.2", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-16.2", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-16.1", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-16.1", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-16.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-16.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-15.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-15.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-14.2", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-14.2", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-14.1", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-14.1", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-14.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-14.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-13.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-13.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-12.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-12.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-11.2", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-11.2", type: "rdsap", construction_option: "C" },
+      { schema: "SAP-Schema-11.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-11.0", type: "rdsap", construction_option: "C" },
+      { schema: "SAP-Schema-10.2", type: "rdsap", construction_option: "C" },
 
-        expect(age_band).to eq(construction_option)
-      end
+      { schema: "SAP-Schema-NI-18.0.0", type: "epc", construction_option: "K" },
+      { schema: "SAP-Schema-NI-17.4", type: "epc", construction_option: "K" },
+      { schema: "SAP-Schema-NI-17.3", type: "epc", construction_option: "K" },
+      { schema: "SAP-Schema-NI-17.2", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-17.2", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-17.1", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-17.1", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-17.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-17.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-16.1", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-16.1", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-16.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-16.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-15.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-15.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-14.2", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-14.2", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-14.1", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-14.1", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-14.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-14.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-13.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-13.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-12.0", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-12.0", type: "sap", construction_option: "1750" },
+      { schema: "SAP-Schema-NI-11.2", type: "rdsap", construction_option: "A" },
+      { schema: "SAP-Schema-NI-11.2", type: "sap", construction_option: "1750" },
+
+      { schema: "RdSAP-Schema-20.0.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-19.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-18.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-17.1", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-17.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-NI-20.0.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-NI-19.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-NI-18.0", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-NI-17.4", type: "epc", construction_option: "K" },
+      { schema: "RdSAP-Schema-NI-17.3", type: "epc", construction_option: "K" },
+    ]
+
+  test_data.each do |data|
+    it "returns the value for construction age band or construction year for #{data[:schema]}" do
+      document = Nokogiri.XML Samples.xml(data[:schema], data[:type])
+      wrapper = ViewModel::Factory.new.create(document.to_s, data[:schema].to_s)
+      age_band = wrapper.view_model.main_dwelling_construction_age_band_or_year
+
+      expect(age_band).to eq(data[:construction_option])
     end
-
-    retrieve_construction_age_band("SAP-Schema-18.0.0", "epc", "1750")
-    retrieve_construction_age_band("SAP-Schema-17.1", "epc", "1750")
-    retrieve_construction_age_band("SAP-Schema-17.0", "epc", "1750")
-    retrieve_construction_age_band("SAP-Schema-16.3", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-16.3", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-16.2", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-16.2", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-16.1", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-16.1", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-16.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-16.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-15.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-15.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-14.2", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-14.2", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-14.1", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-14.1", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-14.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-14.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-13.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-13.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-12.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-12.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-11.2", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-11.2", "rdsap", "C")
-    retrieve_construction_age_band("SAP-Schema-11.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-11.0", "rdsap", "C")
-    retrieve_construction_age_band("SAP-Schema-10.2", "rdsap", "C")
-
-    retrieve_construction_age_band("SAP-Schema-NI-18.0.0", "epc", "K")
-    retrieve_construction_age_band("SAP-Schema-NI-17.4", "epc", "K")
-    retrieve_construction_age_band("SAP-Schema-NI-17.3", "epc", "K")
-    retrieve_construction_age_band("SAP-Schema-NI-17.2", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-17.2", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-17.1", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-17.1", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-17.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-17.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-16.1", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-16.1", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-16.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-16.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-15.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-15.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-14.2", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-14.2", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-14.1", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-14.1", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-14.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-14.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-13.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-13.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-12.0", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-12.0", "sap", "1750")
-    retrieve_construction_age_band("SAP-Schema-NI-11.2", "rdsap", "A")
-    retrieve_construction_age_band("SAP-Schema-NI-11.2", "sap", "1750")
-
-    retrieve_construction_age_band("RdSAP-Schema-20.0.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-19.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-18.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-17.1", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-17.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-NI-20.0.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-NI-19.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-NI-18.0", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-NI-17.4", "epc", "K")
-    retrieve_construction_age_band("RdSAP-Schema-NI-17.3", "epc", "K")
   end
 end
