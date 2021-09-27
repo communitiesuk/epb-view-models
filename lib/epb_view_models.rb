6e2e09ab94b790a3fe70733c90c995a9ef110ab3
Christine Horrocks <splogy2001@hotmail.com>
2021-09-27 13:21:12 +0100

    Update version number

--- lib/epb_view_models.rb
+++ lib/epb_view_models.rb
@@ -5,7 +5,7 @@ loader = Zeitwerk::Loader.for_gem
 loader.setup
 
 module EpbViewModels
-  VERSION = "1.0.7"
+  VERSION = "1.0.8"
 end
 
 # Monkey patching to avoid using ActiveRecord::Type::Boolean.new.cast
