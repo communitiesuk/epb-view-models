062bd6bef771a6d53e3ecb41f549b23a42bbb16d
Barry Halper <barryhalper@gmail.com>
2021-10-04 16:35:04 +0100

    EPBR-1545 Udpdate test to only include a xsd

--- spec/presenter/xsd_spec.rb
+++ spec/presenter/xsd_spec.rb
@@ -1,11 +1,12 @@
 RSpec.describe Presenter::Xsd do
-  context "when loading all the xsd into memory for processing" do
-    let(:export) { described_class.new(xsd_dir_path: "api/schemas/xml/SAP-Schema-18.0.0/SAP/UDT/*Domains.xsd") }
+  context "when reading data from a specific xsd for processing" do
+    let(:export) { described_class.new(xsd_dir_path: "api/schemas/xml/SAP-Schema-18.0.0/SAP/UDT/SAP-Domains.xsd") }
 
     # it "reads all xsd files from the API directory" do
     #   expect(export.read_files).to be_a(Array)
     #   expect(export.read_files).to include(match(/Domains.xsd/))
     # end
+    let(:enums) { export.get_enums_by_type("SAP-BuiltFormCode") }
 
     it "extracts a hash of the enums for a particular node" do
       built_form = { "1" => "Detached",
@@ -14,7 +15,9 @@ RSpec.describe Presenter::Xsd do
                      "4" => "Mid-Terrace",
                      "5" => "Enclosed End-Terrace",
                      "6" => "Enclosed Mid-Terrace" }
-      expect(export.get_enums_by_type("SAP-BuiltFormCode").first).to eq(built_form)
+      expect(enums).to be_a(Array)
+      expect(enums.length).to eq(1)
+      expect(enums.first).to eq(built_form)
     end
   end
 end
