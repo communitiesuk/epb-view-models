f1778f2d88b60b8f169d35edee3318c6d8be992f
Aga Dufrat <a.dufrat@gmail.com>
2021-10-07 14:08:19 +0100

    Add Gateway::XsdFilesGateway

---
+++ lib/gateway/xsd_files_gateway.rb
@@ -0,0 +1,45 @@
+module Gateway
+  class XsdFilesGateway
+    attr_reader :simple_type, :assessment_type, :xsd_dir_path
+
+    def initialize(simple_type:, assessment_type:, xsd_dir_path: "api/schemas/xml/*/")
+      @simple_type = simple_type
+      @assessment_type = assessment_type
+      @xsd_dir_path = xsd_dir_path
+    end
+
+    def schema_version(file)
+      schema_version = file.delete_prefix("api/schemas/xml/").split("/").first
+      sap_defnied_in_rdsap_dir?(file) ? "#{schema_version}/SAP" : schema_version
+    end
+
+    def xsd_files
+      case assessment_type
+      when "SAP"
+        sap_xsd_files
+      when "RdSAP"
+        rdsap_xsd_files
+      when "CEPC"
+        cepc_xsd_files
+      end
+    end
+
+  private
+
+    def sap_defnied_in_rdsap_dir?(file)
+      assessment_type == "SAP" && file.end_with?("SAP-Domains.xsd")
+    end
+
+    def sap_xsd_files
+      Dir.glob("#{xsd_dir_path}*-Domains.xsd")
+    end
+
+    def rdsap_xsd_files
+      Dir.glob("#{xsd_dir_path}SAP-Domains.xsd")
+    end
+
+    def cepc_xsd_files
+      Dir.glob("#{xsd_dir_path}Reported-Data.xsd")
+    end
+  end
+end
---
+++ spec/gateway/xsd_files_gateway_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe Gateway::XsdFilesGateway do
+  subject(:gateway) { described_class.new(simple_type: "SomeNodeName", assessment_type: "RdSAP") }
+
+  it "initializes with simple_type, assessment_type and xsd_dir_path" do
+    expect(gateway.simple_type).to eq("SomeNodeName")
+    expect(gateway.assessment_type).to eq("RdSAP")
+    expect(gateway.xsd_dir_path).to eq("api/schemas/xml/*/")
+  end
+
+  describe "#schema_version" do
+    it "retruns schema version for a file" do
+      file = "api/schemas/xml/CEPC-8.0.0/Reports/Reported-Data.xsd"
+
+      expect(gateway.schema_version(file)).to eq("CEPC-8.0.0")
+    end
+
+    it "appends with /SAP if the SAP definition is in the RdSAP directory" do
+      gateway = described_class.new(simple_type: "SomeNodeName", assessment_type: "SAP")
+      file = "api/schemas/xml/RdSAP-Schema-17.1/RdSAP/UDT/SAP-Domains.xsd"
+
+      expect(gateway.schema_version(file)).to eq("RdSAP-Schema-17.1/SAP")
+    end
+  end
+end
