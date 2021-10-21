86beeb4a546487fb6f4050e993c21284369d5dbb
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2021-10-21 16:26:40 +0100

    add SAX parser for assessment XML (for export to data warehouse)

---
+++ lib/presenter/xml/parser.rb
@@ -0,0 +1,173 @@
+module Presenter
+  module Xml
+    class Parser
+      def initialize(excludes: [], includes: [], bases: [], preferred_keys: {})
+        @excludes = excludes
+        @includes = includes
+        @bases = bases
+        @preferred_keys = preferred_keys
+      end
+
+      def parse(xml)
+        sax_parser.parse xml
+        last_output
+      end
+
+      def sax_parser
+        @assessment_document ||= AssessmentDocument.new(excludes: @excludes, includes: @includes, bases: @bases, preferred_keys: @preferred_keys)
+        @sax_parser ||= Nokogiri::XML::SAX::Parser.new @assessment_document
+      end
+
+    private
+
+      def last_output
+        @assessment_document.output
+      end
+    end
+
+    class AssessmentDocument < Nokogiri::XML::SAX::Document
+      def initialize(excludes: [], includes: [], bases: [], preferred_keys: {})
+        @excludes = excludes
+        @includes = includes
+        @bases = bases
+        @preferred_keys = preferred_keys
+        super()
+      end
+
+      def start_document
+        init!
+        @output = {}
+      end
+
+      def end_document
+        init!
+      end
+
+      def start_element(name, attrs = [])
+        @source_position << name
+        @output_position << as_key(name) unless is_base?(name)
+        @is_excluding = true if @excludes.include?(name)
+        @is_including = true if @includes.include?(name)
+        @attrs = attrs
+        if encountered_position?
+          set_up_list
+        end
+        write_encounter
+      end
+
+      def end_element(name)
+        @source_position.pop
+        @output_position.pop unless is_base?(name)
+        @is_excluding = false if @excludes.include?(name)
+        @is_including = false if @includes.include?(name)
+      end
+
+      def characters(string)
+        if @is_excluding && !@is_including
+          return
+        end
+
+        stripped = string.strip
+        if stripped.length.zero?
+          return
+        end
+
+        value = try_as_number stripped
+
+        if @attrs.length.positive?
+          value = @attrs.to_h.merge({ "value" => value })
+        end
+
+        set_value value
+      end
+
+      attr_reader :output
+
+    private
+
+      def init!
+        @source_position = []
+        @output_position = []
+        @is_excluding = false
+        @is_including = false
+        @attrs = []
+        @encountered = []
+      end
+
+      def as_key(name)
+        if @preferred_keys.key?(name)
+          return @preferred_keys[name]
+        end
+
+        name.downcase.gsub("-", "_")
+      end
+
+      def set_value(value)
+        set_value_with_keys(value, @output_position)
+      end
+
+      def set_value_with_keys(value, keys)
+        prepare_hash keys
+        *key, last = keys
+        key.inject(@output, :fetch)[last] = value
+      end
+
+      def value_at(keys)
+        keys.inject(@output, :fetch)
+      end
+
+      def prepare_hash(keys)
+        cursor = @output
+        keys[..-2].each do |key|
+          cursor[key] = {} unless cursor[key] && cursor[key] != ""
+          cursor = cursor[key]
+        end
+      end
+
+      def is_base?(name)
+        @bases.concat(@excludes).include?(name) || name == @source_position[0]
+      end
+
+      def is_numeric?(string)
+        true if Float(string)
+      rescue StandardError
+        false
+      end
+
+      def try_as_number(string)
+        return string unless is_numeric?(string)
+
+        if string.include?(".")
+          string.to_f
+        else
+          string.to_i
+        end
+      end
+
+      def write_encounter
+        @encountered << source_position_string
+      end
+
+      def encountered_position?
+        @encountered.include? source_position_string
+      end
+
+      def source_position_string
+        @source_position.join(">")
+      end
+
+      def set_up_list
+        return if @output_position.any? { |x| x.is_a? Integer }
+
+        candidate_list = value_at @output_position[..-2]
+        if candidate_list.is_a?(Array)
+          list_index = candidate_list.length
+        else
+          set_value_with_keys([candidate_list.values[0]], @output_position[..-2])
+          list_index = 1
+        end
+        @output_position[-1] = list_index
+      end
+    end
+  end
+end
---
+++ spec/presenter/xml/parser_spec.rb
@@ -0,0 +1,120 @@
+RSpec.describe Presenter::Xml::Parser do
+  context "with no configuration" do
+    it "maps simple XML (with no multiples) to a hash structure" do
+      xml = "<Opening><Node-1>value 1</Node-1><Node-2>value 2</Node-2></Opening>"
+      expected = {
+        "node_1" => "value 1",
+        "node_2" => "value 2",
+      }
+      expect(described_class.new.parse(xml)).to eq expected
+    end
+  end
+
+  context "with bases defined" do
+    let(:parser) { described_class.new bases: %w[Base] }
+
+    it "pulls up nodes under bases into same level as base" do
+      xml = "<Root><Id>ID123</Id><Base><Node-1>value 1</Node-1><Node-2>value 2</Node-2></Base></Root>"
+      expected = {
+        "id" => "ID123",
+        "node_1" => "value 1",
+        "node_2" => "value 2",
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+
+  context "with nodes excluded" do
+    let(:parser) { described_class.new excludes: %w[Node-2] }
+
+    it "excludes XML node names from mapping into output data structure" do
+      xml = "<Root><Id>ID123</Id><Node-1>value 1</Node-1><Node-2>value 2</Node-2><Node-3>value 3</Node-3></Root>"
+      expected = {
+        "id" => "ID123",
+        "node_1" => "value 1",
+        "node_3" => "value 3",
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+
+  context "with nodes included on top of excludes" do
+    let(:parser) { described_class.new excludes: %w[Exclude], includes: %w[Include-Me] }
+
+    it "excludes XML nodes under an exclude but including any includes within it" do
+      xml = "<Root><Id>ID123</Id><Exclude><Assessment-Id>ID456</Assessment-Id><Include-Me>me!</Include-Me></Exclude></Root>"
+      expected = {
+        "id" => "ID123",
+        "include_me" => "me!",
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+
+  context "with preferred keys given" do
+    let(:parser) { described_class.new preferred_keys: { "Internal-Name" => "external_name" } }
+
+    it "uses a preferred key when given a node name that has one" do
+      xml = "<Root><Id>ID123</Id><Internal-Name>blue</Internal-Name></Root>"
+      expected = {
+        "id" => "ID123",
+        "external_name" => "blue",
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+
+  context "with a list of nodes within a reasonable deep hierarchy" do
+    let(:parser) { described_class.new }
+
+    it "recognises a list and maps it into the data structure" do
+      xml = "<Root><Subject><Sub-Subject><Rooms><Room><Id>ROOM1</Id><Name>Room 1</Name></Room><Room><Id>ROOM2</Id><Name>Room 2</Name></Room></Rooms></Sub-Subject></Subject></Root>"
+      expected = {
+        "subject" => {
+          "sub_subject" => {
+            "rooms" => [
+              {
+                "id" => "ROOM1",
+                "name" => "Room 1",
+              },
+              {
+                "id" => "ROOM2",
+                "name" => "Room 2",
+              },
+            ],
+          },
+        },
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+
+  context "with node values containing numeric values" do
+    let(:parser) { described_class.new }
+
+    it "recognises values that can be numbers and maps them to floats or integers depending on decimal point" do
+      xml = "<Root><Not-Quite-Numeric>123ABC</Not-Quite-Numeric><Floaty>13.45</Floaty><Inty>42</Inty></Root>"
+      expected = {
+        "not_quite_numeric" => "123ABC",
+        "floaty" => 13.45,
+        "inty" => 42,
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+
+  context "with nodes containing attributes" do
+    let(:parser) { described_class.new }
+
+    it "maps these nodes as a hash containing the node's attributes plus a value attribute with the value" do
+      xml = '<Root><Money-Amount currency="GBP">139.99</Money-Amount></Root>'
+      expected = {
+        "money_amount" => {
+          "currency" => "GBP",
+          "value" => 139.99,
+        },
+      }
+      expect(parser.parse(xml)).to eq expected
+    end
+  end
+end
