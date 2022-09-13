0da1fa937640ddb8e1b9d060980fc3837a34564c
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2022-09-13 09:54:36 +0100

    make 1.0.24 release

--- lib/epb_view_models.rb
+++ lib/epb_view_models.rb
@@ -5,7 +5,7 @@ loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
 module EpbViewModels
-  VERSION = "1.0.23"
+  VERSION = "1.0.24"
 end
 
 # Monkey patching to avoid using ActiveRecord::Type::Boolean.new.cast
