0e49458124adee406f857c26e83a534697b5d86e
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2024-03-14 16:17:13 +0000

    Release version 2.0.2.pre2
    Prerelease for testing lodgement of large XML documents.

--- lib/epb_view_models.rb
+++ lib/epb_view_models.rb
@@ -5,7 +5,7 @@ loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
 module EpbViewModels
-  VERSION = "2.0.2.pre"
+  VERSION = "2.0.2.pre2"
 end
 
 # Monkey patching to avoid using ActiveRecord::Type::Boolean.new.cast
