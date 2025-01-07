cc6c96a33942ecedd122e0d6d32946ae9e78e32f
christinehorrocks <christine.horrocks@communities.gov.uk>
2025-01-07 11:39:57 +0000

    EPBR-8023 Refactor RdSAP wrapper to hash test for England and Wales
    Add the new format of wrapper tests for to_hash method on England and Wales rdsaps

--- spec/view_model/rdsap_wrapper_new_spec.rb
+++ spec/view_model/rdsap_wrapper_new_spec.rb
@@ -4,7 +4,24 @@ RSpec.describe ViewModel::RdSapWrapper do
   context "when calling the RdSAP wrapper for a valid schema" do
     it "returns the expected assertion" do
       schema_tests = [
-        { schema: "RdSAP-Schema-21.0.0", type: "epc", method_called: :to_hash }
+        { schema: "RdSAP-Schema-21.0.0", type: "epc", method_called: :to_hash },
+        { schema: "RdSAP-Schema-20.0.0", type: "epc", method_called: :to_hash },
+        { schema: "RdSAP-Schema-19.0", type: "epc", method_called: :to_hash },
+        { schema: "RdSAP-Schema-18.0", type: "epc", method_called: :to_hash },
+        { schema: "RdSAP-Schema-17.1", type: "epc", method_called: :to_hash },
+        { schema: "RdSAP-Schema-17.0", type: "epc", method_called: :to_hash },
+        { schema: "SAP-Schema-16.3", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-16.2", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-16.1", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-16.0", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-15.0", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-14.2", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-14.1", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-14.0", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-13.0", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-12.0", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-11.2", type: "rdsap", method_called: :to_hash },
+        { schema: "SAP-Schema-11.0", type: "rdsap", method_called: :to_hash },
       ]
 
       schema_tests.each do |schema|
