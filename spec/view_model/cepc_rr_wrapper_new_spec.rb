e43323714a9fec33f2b3db07acc6ed9d971c8298
Eleanor Mollett <eleanor.mollett@gmail.com>
2025-01-28 10:25:00 +0000

    EPBR-8165 Refactor the cepc-rr wrapper specs for to_hash

---
+++ spec/view_model/cepc_rr_wrapper_new_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../wrapper_test_helper"
+
+RSpec.describe ViewModel::CepcRrWrapper do
+  context "when calling the CEPC-RR wrapper for a valid schema" do
+    it "returns the expected assertion for the to_hash method" do
+      schema_tests = [
+        { schema: "CEPC-8.0.0", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-NI-8.0.0", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-7.1", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-7.0", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-6.0", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-5.1", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-5.0", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-4.0", type: "cepc-rr", method_called: :to_hash },
+        { schema: "CEPC-3.1", type: "cepc-rr", method_called: :to_hash },
+      ]
+
+      schema_tests.each do |schema|
+        test_wrapper(schema)
+      end
+    end
+  end
+end
