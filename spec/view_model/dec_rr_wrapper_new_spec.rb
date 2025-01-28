b098276f7ae15ab39045f520a602fa44542a9fed
Eleanor Mollett <eleanor.mollett@gmail.com>
2025-01-28 13:51:09 +0000

    EPBR-8165 Refactor the dec-rr wrapper specs for to_hash

---
+++ spec/view_model/dec_rr_wrapper_new_spec.rb
@@ -0,0 +1,31 @@
+require_relative "../wrapper_test_helper"
+
+RSpec.describe ViewModel::DecRrWrapper do
+  context "when calling the DEC-RR wrapper for a valid schema" do
+    it "returns the expected assertion for the to_hash method" do
+      schema_tests = [
+        { schema: "CEPC-8.0.0", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-8.0.0", type: "dec-rr-large-building", method_called: :to_hash },
+        { schema: "CEPC-NI-8.0.0", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-7.1", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-7.1", type: "dec-rr-ni", method_called: :to_hash },
+        { schema: "CEPC-7.0", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-7.0", type: "dec-rr-ni", method_called: :to_hash },
+        { schema: "CEPC-6.0", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-6.0", type: "dec-rr-ni", method_called: :to_hash },
+        { schema: "CEPC-5.1", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-5.1", type: "dec-rr-ni", method_called: :to_hash },
+        { schema: "CEPC-5.0", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-5.0", type: "dec-rr-ni", method_called: :to_hash },
+        { schema: "CEPC-4.0", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-4.0", type: "dec-rr-ni", method_called: :to_hash },
+        { schema: "CEPC-3.1", type: "dec-rr", method_called: :to_hash },
+        { schema: "CEPC-3.1", type: "dec-rr-ni", method_called: :to_hash },
+      ]
+
+      schema_tests.each do |schema|
+        test_wrapper(schema)
+      end
+    end
+  end
+end
