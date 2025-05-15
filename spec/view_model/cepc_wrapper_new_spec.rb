8dbac46bf0cb25caab9c6985d3914dd712f61a96
christinehorrocks <christine.horrocks@communities.gov.uk>
2025-05-15 16:09:44 +0100

    EPBR-8958 Add expiry date to CEPC-S-7.1 and Update version number

--- spec/view_model/cepc_wrapper_new_spec.rb
+++ spec/view_model/cepc_wrapper_new_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe ViewModel::CepcWrapper do
   context "when calling the CEPC wrapper for a valid schema" do
     it "returns the expected assertion for the to_hash method", :aggregate_failures do
       schema_tests = [
-        # { schema: "CEPC-8.0.0", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-NI-8.0.0", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-7.1", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-7.0", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-6.0", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-5.1", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-5.0", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-4.0", type: "cepc", method_called: :to_hash },
-        # { schema: "CEPC-3.1", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-8.0.0", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-NI-8.0.0", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-7.1", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-7.0", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-6.0", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-5.1", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-5.0", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-4.0", type: "cepc", method_called: :to_hash },
+        { schema: "CEPC-3.1", type: "cepc", method_called: :to_hash },
         { schema: "CEPC-S-7.1", type: "cepc", method_called: :to_hash },
       ]
 
