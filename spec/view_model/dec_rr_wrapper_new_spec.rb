e76ada65c5aaa159545a3ca46ff716dd9418c461
christinehorrocks <christine.horrocks@communities.gov.uk>
2025-06-10 10:24:06 +0100

    EPBR-5008 Add tests for DEC rr to_report and update version

--- spec/view_model/dec_rr_wrapper_new_spec.rb
+++ spec/view_model/dec_rr_wrapper_new_spec.rb
@@ -27,5 +27,22 @@ RSpec.describe ViewModel::DecRrWrapper do
         expect { test_wrapper(schema) }.not_to raise_error
       end
     end
+
+    it "returns the expected assertion for the to_report method", :aggregate_failures do
+      schema_tests = [
+        { schema: "CEPC-NI-8.0.0", type: "dec-rr", method_called: :to_report },
+        { schema: "CEPC-7.1", type: "dec-rr-ni", method_called: :to_report },
+        { schema: "CEPC-7.0", type: "dec-rr-ni", method_called: :to_report },
+        { schema: "CEPC-6.0", type: "dec-rr-ni", method_called: :to_report },
+        { schema: "CEPC-5.1", type: "dec-rr-ni", method_called: :to_report },
+        { schema: "CEPC-5.0", type: "dec-rr-ni", method_called: :to_report },
+        { schema: "CEPC-4.0", type: "dec-rr-ni", method_called: :to_report },
+        { schema: "CEPC-3.1", type: "dec-rr-ni", method_called: :to_report },
+      ]
+
+      schema_tests.each do |schema|
+        expect { test_wrapper(schema) }.not_to raise_error
+      end
+    end
   end
 end
