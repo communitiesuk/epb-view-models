e725421b676c81aa602f1a9ff9c698332e0c8a4c
christinehorrocks <christine.horrocks@communities.gov.uk>
2026-02-12 11:21:18 +0000

    EPBR-9274 Update version number

--- lib/epb_view_models.rb
+++ lib/epb_view_models.rb
@@ -5,7 +5,7 @@ loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
 module EpbViewModels
-  VERSION = "2.2.3"
+  VERSION = "2.2.4"
 end
 
 # Monkey patching to avoid using ActiveRecord::Type::Boolean.new.cast
