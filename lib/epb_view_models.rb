545dc694a33d2110a79db48f2e0a45a38ee2c9da
Eleanor Mollett <eleanor.mollett@gmail.com>
2026-02-24 13:04:09 +0000

    Update gem

--- lib/epb_view_models.rb
+++ lib/epb_view_models.rb
@@ -5,7 +5,7 @@ loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
 module EpbViewModels
-  VERSION = "2.2.7"
+  VERSION = "2.2.8"
 end
 
 # Monkey patching to avoid using ActiveRecord::Type::Boolean.new.cast
