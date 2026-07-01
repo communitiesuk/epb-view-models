939c0ee2bfcba459595eec84e0550ab61eb22a50
christinehorrocks <christine.horrocks@communities.gov.uk>
2026-07-01 14:10:33 +0100

    EPBR-9249 Add extra tests for crazy name spacing in RdSAP-Schema-S-16.1

--- spec/view_model/rdsap_wrapper_new_spec.rb
+++ spec/view_model/rdsap_wrapper_new_spec.rb
@@ -49,6 +49,7 @@ RSpec.describe ViewModel::RdSapWrapper do
         { schema: "RdSAP-Schema-S-18.0", type: "epc", method_called: :to_hash },
         { schema: "RdSAP-Schema-S-17.00", type: "epc", method_called: :to_hash },
         { schema: "RdSAP-Schema-S-16.1", type: "epc", method_called: :to_hash },
+        { schema: "RdSAP-Schema-S-16.1", type: "epc_2", method_called: :to_hash },
       ]
 
       schema_tests.each do |schema|
