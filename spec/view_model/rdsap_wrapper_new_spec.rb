325d7b3b9ac55496ea73f164641f18d1c329e36c
christinehorrocks <christine.horrocks@communities.gov.uk>
2026-08-05 09:40:35 +0100

    EPBR-9257 Update the RdSAP-Schema-S-15.0 test data

--- spec/view_model/rdsap_wrapper_new_spec.rb
+++ spec/view_model/rdsap_wrapper_new_spec.rb
@@ -106,6 +106,7 @@ RSpec.describe ViewModel::RdSapWrapper do
         { schema: "RdSAP-Schema-S-18.0", type: "epc", method_called: :to_certificate_summary },
         { schema: "RdSAP-Schema-S-17.00", type: "epc", method_called: :to_certificate_summary },
         { schema: "RdSAP-Schema-S-16.1", type: "epc", method_called: :to_certificate_summary },
+        { schema: "RdSAP-Schema-S-15.0", type: "epc", method_called: :to_certificate_summary },
       ]
 
       schema_tests.each do |schema|
